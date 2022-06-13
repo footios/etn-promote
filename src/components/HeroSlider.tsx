2c0c4e36584579d167ab07a8a3ca6f69b037bca7
foti <footios76@gmail.com>
2022-06-13 12:22:45 +0300

    Add heroSlider caption

--- src/components/HeroSlider.tsx
+++ src/components/HeroSlider.tsx
@@ -1,6 +1,8 @@
+/* eslint-disable react/no-unescaped-entities */
 /* eslint-disable @typescript-eslint/no-var-requires */
 import React, { FC } from "react";
 import { Carousel } from "react-bootstrap";
+import "../styles/sass/components/heroSlider/heroSlider.css";
 
 const image1 = require("../assets/images/slide1.jpeg");
 const image2 = require("../assets/images/slide2.jpeg");
@@ -18,48 +20,59 @@ const HeroSlider: FC<Props> = ({ language }) => {
         <Carousel>
           <Carousel.Item interval={1000}>
             <img className="d-block w-100" src={image1} alt="First slide" />
-            {/* <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption> */}
+            <Carousel.Caption>
+              <h2>Μάθε τα τῆς Πίστεώς σου!</h2>
+              <h3>
+                "Ἐν ἀρχῇ ἦν ὁ Λόγος, καὶ ὁ Λόγος ἦν πρὸς τὸν Θεόν, καὶ Θεὸς ἦν ὁ
+                Λόγος." (Ἰωάν. 1, 1)
+              </h3>
+            </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item interval={500}>
             <img className="d-block w-100" src={image2} alt="Second slide" />
-            {/* <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption> */}
+            <Carousel.Caption>
+              <h2>Διάβασε τά Ἱερά Κείμενα!</h2>
+              <h3>"Ἆρά γε γινώσκεις ἃ ἀναγινώσκεις" (Πραξ. 8,30)</h3>
+            </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
             <img className="d-block w-100" src={image3} alt="Third slide" />
-            {/* <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-        </Carousel.Caption> */}
+            <Carousel.Caption>
+              <h2>Ὁ Χριστός σέ ἀγαπάει!</h2>
+              <h3>
+                "Ὁ ζητῶν εὑρίσκει καὶ τῷ κρούοντι ἀνοιγήσεται" (Ματ. 7, 8)
+              </h3>
+            </Carousel.Caption>
           </Carousel.Item>
         </Carousel>
       ) : (
         <Carousel>
           <Carousel.Item interval={1000}>
             <img className="d-block w-100" src={image1en} alt="First slide" />
-            {/* <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption> */}
+            <Carousel.Caption>
+              <h2>Learn about your Faith!</h2>
+              <h3>
+                "In the beginning was the Word, and the Word was with God, and
+                the Word was God." (John. 1, 1)
+              </h3>
+            </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item interval={500}>
             <img className="d-block w-100" src={image2en} alt="Second slide" />
-            {/* <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption> */}
+            <Carousel.Caption>
+              <h2>Read the Holy Scriptures!</h2>
+              <h3>"Do you understand what you are reading?" (Acts. 8, 30)</h3>
+            </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
             <img className="d-block w-100" src={image3en} alt="Third slide" />
-            {/* <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-        </Carousel.Caption> */}
+            <Carousel.Caption>
+              <h2>Jesus Loves you!</h2>
+              <h3>
+                "He who seeks finds; and to him who knocks, the door will be
+                opened." (Mat. 7, 8)
+              </h3>
+            </Carousel.Caption>
           </Carousel.Item>
         </Carousel>
       )}
