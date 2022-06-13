26e0d4b31fbc6ddd1d763046c16d8ccba1586f74
foti <footios76@gmail.com>
2022-06-13 09:04:42 +0300

    Info text corrections

--- src/constatnts/infoTextEN.tsx
+++ src/constatnts/infoTextEN.tsx
@@ -3,22 +3,13 @@ import { ButtonMailTo } from "../components";
 
 const infoTextGR = (
   <div>
-    This app is an orthodox Christian quiz with 264 questions which are followed
-    by detailed answers with references and quotations in the Bible and texts of
-    the Saints.
-    <br />
-    <br />
-    It has two game modes: Multiple Choices and True - False.
-    <br />
-    <br />
-    In each mode you will find 33 questions in the following categories: New
-    Testament, Old Testament, Life of Saints and Holy spiritual life. In
-    addition, there is also a Copmilation, in which you can play with as many
-    categories as you want simultaneously.
+    Further, there is a personal screen where you can see your account details,
+    your points, your rewards, the number of answers you have given, and your
+    success rate.
     <br />
     <br />
     On the screen with the winners you see the ranking of all the players
-    depending on the points or prizes.
+    depending on the points or rewards.
     <br />
     In the settings, if you have registered, you can change your name, your
     email address, your password, or delete it your account.
